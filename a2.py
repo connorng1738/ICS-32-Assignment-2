8ec033dc13f89caa6ffecfc2524b0d2692b71a33
Connor Ng <conner@dhcp-10-8-007-099.mobile.reshsg.uci.edu>
2025-04-26 16:31:22 -0700

    second commit

--- a2.py
+++ a2.py
@@ -2,3 +2,6 @@ from game import *
 from ui import play_game
 if __name__ == "__main__":
     play_game()
+
+
+    
--- ui.py
+++ ui.py
@@ -1,9 +1,17 @@
 from game import *
 
 def play_game():
+
+    command = int(input())
     rows = int(input("Rows:"))
     cols = int(input("Columns:"))
 
     game = Game(rows, cols)
+    
+    while True:
+        print(game.create_field())
 
-    print(game.create_field())
+        if command == 'Q':
+            break
+        if command == 'F':
+            pass
